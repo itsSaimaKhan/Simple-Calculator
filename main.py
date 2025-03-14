08af42f19d3f40f517457f488557043b1ac19094
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-14 23:48:14 +0500

    main

---
+++ main.py
@@ -0,0 +1,37 @@
+my_string_1 : str = "hello"
+my_string_2 : str = "world"
+my_string_3 : str = my_string_1 + "" + my_string_2
+
+print(my_string_1 + my_string_2)
+print(my_string_3[-1])
+print(len(my_string_3))
+my_string_3 : str = "hel lo- wo-rld,"
+print(my_string_3[0:11:2])
+
+print( [method for method in dir(str) if "-" not in method])
+print(my_string_3.upper())
+print(my_string_3.lower())
+
+print(my_string_3.split())
+print(my_string_3.split("L"))
+
+my_string_3 : str = ""
+
+
+print(my_string_3)
+
+my_string_3.replace("hello", "hi")
+print(my_string_3)
+
+my_string: str = "hello, world! hello, pakistan"
+starting-index = my_string.find("hello")
+starting_index_2 = starting("hello", starting_index + 1)
+print(starting_index_2)
+
+
+print("Substring to search = ", "Hello")
+
+
+
+
+
