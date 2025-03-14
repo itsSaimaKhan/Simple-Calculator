2cce2093b903a4e8a7c85e731f9025b859c9aec9
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-14 23:50:42 +0500

    main

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ my_string_1 : str = "hello"
 my_string_2 : str = "world"
 my_string_3 : str = my_string_1 + "" + my_string_2
 
-# print(my_string_1 + my_string_2)
-# print(my_string_3[-1])
-# print(len(my_string_3))
-# my_string_3 : str = "hel lo- wo-rld,"
-# print(my_string_3[0:11:2])
+print(my_string_1 + my_string_2)
+print(my_string_3[-1])
+print(len(my_string_3))
+my_string_3 : str = "hel lo- wo-rld,"
+print(my_string_3[0:11:2])
 
 # print( [method for method in dir(str) if "-" not in method])
 # print(my_string_3.upper())
