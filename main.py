34eb3714a17ac6cabe351fde9f2f26547f071260
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-14 23:49:26 +0500

    main

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ print(len(my_string_3))
 my_string_3 : str = "hel lo- wo-rld,"
 print(my_string_3[0:11:2])
 
-print( [method for method in dir(str) if "-" not in method])
-print(my_string_3.upper())
-print(my_string_3.lower())
+# print( [method for method in dir(str) if "-" not in method])
+# print(my_string_3.upper())
+# print(my_string_3.lower())
 
-print(my_string_3.split())
-print(my_string_3.split("L"))
+# print(my_string_3.split())
+# print(my_string_3.split("L"))
 
-my_string_3 : str = ""
+# my_string_3 : str = ""
 
 
 # print(my_string_3)
