23939894d4f4fd20120733fe8702e641ab33aab2
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-16 23:37:42 +0500

    main

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ my_string_3 : str = ""
 # print(my_string_3)
 
 # my_string: str = "hello, world! hello, pakistan"
-# starting-index = my_string.find("hello")
-# starting_index_2 = starting("hello", starting_index + 1)
+starting-index = my_string.find("hello")
+starting_index_2 = starting("hello", starting_index + 1)
 print(starting_index_2)
 
 
