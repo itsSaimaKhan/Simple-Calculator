df1de77fad54fbcedbd2ea69ec212b486685424b
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-18 23:50:17 +0500

    main

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ print(len(my_string_3))
 # my_string_3.replace("hello", "hi")
 # print(my_string_3)
 
-# my_string: str = "hello, world! hello, pakistan"
-# starting-index = my_string.find("hello")
+my_string: str = "hello, world! hello, pakistan"
+starting-index = my_string.find("hello")
 starting_index_2 = starting("hello", starting_index + 1)
 print(starting_index_2)
 
